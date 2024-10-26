d249ba5930f982cb60105aad75ddb9076f81a889
Tuan Duong <tuanduong@MBP-Tuan.local>
2024-10-26 01:43:35 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink, useLocation, useSearchParams } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink, useLocation, useSearchParams } from 'react-router-dom';
 import './styles.css'; // Import CSS styles
 
 // Tabs Component
